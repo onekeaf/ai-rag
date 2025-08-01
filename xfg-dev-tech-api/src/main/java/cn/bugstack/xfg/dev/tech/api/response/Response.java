ea7906fa976b2a7a860abf854ce4c14b8d6aab38
oneleaf <2840599665@qq.com>
2025-08-01 20:43:01 +0800

    feat(dev-tech): 添加分析 Git仓库功能
    - 在 IRAGService 接口中添加 analyzeGitRepo 方法
    - 实现 Git 仓库克隆、文件遍历解析、文档上传至向量数据库的功能
    - 优化日志输出和异常处理
    - 删除临时克隆的目录
    - 更新 Redis 缓存中的知识库列表

---
+++ xfg-dev-tech-api/src/main/java/cn/bugstack/xfg/dev/tech/api/response/Response.java
@@ -0,0 +1,24 @@
+package cn.bugstack.xfg.dev.tech.api.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+/**
+ * @author oneleaf
+ * @description
+ * @create 下午8:17
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Response<T> implements Serializable {
+
+    private String code;
+    private String info;
+    private T data;
+}
